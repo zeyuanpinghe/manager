ce3506698f0042abfcd510a9a041efb06f36baf3
chzh88 <chzh55@163.com>
2019-07-01 23:23:10 +0800

    run

--- src/main/java/com/manager/woods/sys/redis/SysConfigRedis.java
+++ src/main/java/com/manager/woods/sys/redis/SysConfigRedis.java
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
+
 
 package com.manager.woods.sys.redis;
 
@@ -20,7 +14,7 @@ import org.springframework.stereotype.Component;
  *
  * @author Mark sunlightcs@gmail.com
  */
-@Component
+//@Component
 public class SysConfigRedis {
     @Autowired
     private RedisUtils redisUtils;
